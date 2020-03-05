27a40baaec93620ab24906f12aff08ccadcb7981
Laura Shapiro <laurashapiro01@gmail.com>
2020-03-05 15:08:47 -0500

    express get endpoint works with raw json

--- superback/database.js
+++ superback/database.js
@@ -24,17 +24,54 @@ Object.keys(columns).forEach(item=>{
         longString += ", "
     }
 })
-console.log(longString)
+// console.log(longString)
 
-pool.connect();
+// pool.connect();
 // pool.query(longString);
+
+
+let ids = chars.map(item=> item.id).sort((a,b)=> a>b);
+let newIds = 1; 
+
+let newSupers = chars.map(item=>{
+    if (!item.id) {
+        if (!ids.includes(newIds)) {
+            item.id = newIds;
+            ids.push(newIds)
+            newIds +=1 
+        }
+    }
+    if (!item.name) {
+        item.name = item.slug.split('-')[1];
+    } 
+    item.name = item.name.split(' ').map(item=> item[0].toUpperCase() + item.slice(1).toLowerCase()).join(' ');
+
+    return item;
+})
+
+
+  newSupers = newSupers.sort((a,b)=>{
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
+    
+  })
+
 
 
 let rowStrings = [];
 let parents = [ "powerstats", "appearance", "biography", "work", "connections", "images"]
 
 
-chars.forEach((item, index)=>{
+ newSupers.forEach((item, index)=>{
 
     let myStr = []
     let query = "INSERT INTO supers(";
@@ -52,7 +89,8 @@ chars.forEach((item, index)=>{
 
     })
 
-    query += ") VALUES(" 
+    query = query.slice(0, query.length -2)
+    query += ")VALUES(" 
 
     myStr.forEach(item=>{
         if (typeof item === "string") {
@@ -64,7 +102,7 @@ chars.forEach((item, index)=>{
         }
     })
 
-    query = query.slice(0, query.length -2)
+    query = query.slice(0, query.length -1)
     query += ");"
         if (index == 5) {
 
--- superback/index.js
+++ superback/index.js
@@ -5,20 +5,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 var cors = require('cors')
 app.use(cors())
+const chars = require('./characters-corrupt.json')
+
+
+let ids = chars.map(item=> item.id).sort((a,b)=> a>b);
+let newIds = 1; 
+let newSupers = chars.map(item=>{
+    if (!item.id) {
+        if (!ids.includes(newIds)) {
+            item.id = newIds;
+            ids.push(newIds)
+            newIds +=1 
+        }
+    }
+    if (!item.name) {
+        item.name = item.slug.split('-')[1];
+    } 
+    item.name = item.name.split(' ').map(item=> item[0].toUpperCase() + item.slice(1).toLowerCase()).join(' ');
+
+    return item;
+})
+
 
 app.listen(8000, () =>
   console.log(`Example app listening on port 8000`),
 );
 
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+    // return chars; 
+  return res.send(newSupers);
 });
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+
+app.get('/:id', (req, res) => {
+    let thisChar = newSupers.filter(item=> item.id == req.params.id )[0]
+
+  return res.send(thisChar);
 });
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
+// app.post('/', (req, res) => {
+//   return res.send('Received a POST HTTP method');
+// });
+// app.put('/', (req, res) => {
+//   return res.send('Received a PUT HTTP method');
+// });
 
 
 app.post('/game', (req, res) =>{
